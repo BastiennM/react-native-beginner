cf4ffd30a195ba2d71733ae1f43205f03fd9aff1
Metais Bastien <bastienmetais79@gmail.com>
2024-10-16 14:23:07 +0200

    init

---
+++ src/views/Wishlist.tsx
@@ -0,0 +1,12 @@
+import {Text, View} from 'react-native';
+import * as React from 'react';
+
+function Wishlist() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Wislist Screen</Text>
+        </View>
+    );
+}
+
+export default Wishlist;

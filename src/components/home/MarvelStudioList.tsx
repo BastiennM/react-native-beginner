5e24d8ade04c1ee3453c2fb19a557b5cd0173db1
Metais Bastien <bastienmetais79@gmail.com>
2024-11-05 16:45:08 +0100

    style: format code and update imports

--- src/components/home/MarvelStudioList.tsx
+++ src/components/home/MarvelStudioList.tsx
@@ -1,7 +1,7 @@
-import {View, StyleSheet, ScrollView} from "react-native";
-import {useTheme} from "../../providers/theme_provider";
-import HeaderList from "./HeaderList";
-import MoviePoster from "../MoviePoster";
+import {View, StyleSheet, ScrollView} from 'react-native';
+import {useTheme} from '../../providers/theme_provider';
+import HeaderList from './HeaderList';
+import MoviePoster from '../MoviePoster';
 
 const MarvelStudioList = () => {
     const {theme} = useTheme();
@@ -9,34 +9,34 @@ const MarvelStudioList = () => {
     const marvelMovies = [
         {
             id: 1,
-            title: "Thor: Love and Thunder",
-            imageUrl: "https://image.tmdb.org/t/p/w500/pIkRyD18kl4FhoCNQuWxWu5cBLM.jpg"
+            title: 'Thor: Love and Thunder',
+            imageUrl: 'https://image.tmdb.org/t/p/w500/pIkRyD18kl4FhoCNQuWxWu5cBLM.jpg',
         },
         {
             id: 2,
-            title: "Doctor Strange in the Multiverse of Madness",
-            imageUrl: "https://image.tmdb.org/t/p/w500/9Gtg2DzBhmYamXBS1hKAhiwbBKS.jpg"
+            title: 'Doctor Strange in the Multiverse of Madness',
+            imageUrl: 'https://image.tmdb.org/t/p/w500/9Gtg2DzBhmYamXBS1hKAhiwbBKS.jpg',
         },
         {
             id: 3,
-            title: "Spider-Man: No Way Home",
-            imageUrl: "https://image.tmdb.org/t/p/w500/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg"
+            title: 'Spider-Man: No Way Home',
+            imageUrl: 'https://image.tmdb.org/t/p/w500/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg',
         },
         {
             id: 4,
-            title: "Eternals",
-            imageUrl: "https://image.tmdb.org/t/p/w500/bcCBq9N1EMo3daNIjWJ8kYvrQm6.jpg"
+            title: 'Eternals',
+            imageUrl: 'https://image.tmdb.org/t/p/w500/bcCBq9N1EMo3daNIjWJ8kYvrQm6.jpg',
         },
         {
             id: 5,
-            title: "Black Widow",
-            imageUrl: "https://image.tmdb.org/t/p/w500/qAZ0pzat24kLdO3o8ejmbLxyOac.jpg"
-        }
+            title: 'Black Widow',
+            imageUrl: 'https://image.tmdb.org/t/p/w500/qAZ0pzat24kLdO3o8ejmbLxyOac.jpg',
+        },
     ];
 
     return (
         <View style={styles.container}>
-            <HeaderList headerLabel={"Marvel Studios"} seeMoreLabel={"See more"}/>
+            <HeaderList headerLabel={'Marvel Studios'} seeMoreLabel={'See more'}/>
             <ScrollView
                 horizontal
                 showsHorizontalScrollIndicator={false}
@@ -53,7 +53,7 @@ const MarvelStudioList = () => {
             </ScrollView>
         </View>
     );
-}
+};
 
 const createStyles = (theme: any) => StyleSheet.create({
     container: {
@@ -65,7 +65,7 @@ const createStyles = (theme: any) => StyleSheet.create({
     },
     movieContainer: {
         marginRight: 16,
-    }
+    },
 });
 
 export default MarvelStudioList;
